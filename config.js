2c5932a8440b7cb274e2f9e000f52c0daeab4a7d
HANSTKW <107929352+HANSTKW@users.noreply.github.com>
2022-06-21 19:51:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6289647417373'] // nomer anda
+global.owner = ['6283136335310'] // nomer anda
 global.mods = [] // Want some help?
-global.prems = ['6289999999999'] // Premium user has unlimited limit
+global.prems = ['6283136335310'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Wabot-aq02'
+global.author = 'Wabot-kyx2'
 
 global.multiplier = 69 // The higher, The harder levelup
 
